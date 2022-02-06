3861d40b61ba13a3aa42caa783de7d4826d89ff9
Martin Ertsås <martiert@gmail.com>
2022-02-06 21:12:06 +0100

    moghedien: Generalize mount points

--- machines/laptop.nix
+++ machines/laptop.nix
@@ -7,6 +7,7 @@ in
 {
   imports = [
     ./x86_64.nix
+    ./mountpoints.nix
     ../users/martin.nix
     ../users/root.nix
     ../secrets/moghedien_network.nix
@@ -18,34 +19,15 @@ in
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/747150e3-46c0-41a2-8735-3c042dec1d2d";
-    fsType = "ext4";
+  martiert.mountpoints = {
+    keyDisk.keyFile = "luks/moghedien.key";
+    root = {
+      encryptedDevice = "/dev/disk/by-uuid/90041fed-c9a4-4139-a61d-76c6c4aca100";
+      device = "/dev/disk/by-uuid/747150e3-46c0-41a2-8735-3c042dec1d2d";
+    };
+    boot = "/dev/disk/by-uuid/C414-3256";
+    swap = "/dev/disk/by-partuuid/813b7f11-8581-4af9-839c-c46e0be03f39";
   };
-
-  boot.initrd.luks.devices."root" = {
-    device = "/dev/disk/by-uuid/90041fed-c9a4-4139-a61d-76c6c4aca100";
-    keyFile = "${keyDir}/luks/moghedien.key";
-    preLVM = false;
-    fallbackToPassword = true;
-    preOpenCommands = ''
-      mkdir "${keyDir}"
-      waitDevice "${disk}"
-      mount "${disk}" "${keyDir}"
-    '';
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/C414-3256";
-    fsType = "vfat";
-  };
-
-  swapDevices = [
-    {
-      device = "/dev/disk/by-partuuid/813b7f11-8581-4af9-839c-c46e0be03f39";
-      randomEncryption = true;
-    }
-  ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 }
---
+++ machines/mountpoints.nix
@@ -0,0 +1,75 @@
+{ config, lib, ... }:
+
+with lib;
+
+let
+  cfg = config.martiert.mountpoints;
+  keyDir = "/keys";
+in {
+  options.martiert.mountpoints = {
+    keyDisk = mkOption {
+      type = types.submodule {
+        options = {
+          disk = mkOption {
+            type = types.str;
+            default = "/dev/disk/by-label/keys";
+            description = "disk to mount for luks decryption";
+          };
+          keyFile = mkOption {
+            type = types.str;
+            description = "keyfile to use";
+          };
+        };
+      };
+    };
+    root = mkOption {
+      type = types.submodule {
+        options = {
+          encryptedDevice = mkOption {
+            type = types.str;
+            description = "device to unlock";
+          };
+          device = mkOption {
+            type = types.str;
+            description = "decrypted device to mount";
+          };
+        };
+      };
+    };
+    boot = mkOption {
+      type = types.str;
+      description = "device to mount to /boot";
+    };
+    swap = mkOption {
+      type = types.str;
+      description = "swap device to mount";
+    };
+
+  };
+
+  config = {
+    boot.initrd.luks.devices."root" = {
+      device = cfg.root.encryptedDevice;
+      keyFile = "${keyDir}/${cfg.keyDisk.keyFile}";
+      preLVM = false;
+      fallbackToPassword = true;
+      preOpenCommands = ''
+        mkdir "${keyDir}"
+        waitDevice "${cfg.keyDisk.disk}"
+        mount "${cfg.keyDisk.disk}" "${keyDir}"
+      '';
+    };
+    fileSystems."/" = {
+      device = cfg.root.device;
+      fsType = "ext4";
+    };
+    fileSystems."/boot" = {
+      device = cfg.boot;
+      fsType = "vfat";
+    };
+    swapDevices = [{
+      device = cfg.swap;
+      randomEncryption = true;
+    }];
+  };
+}
