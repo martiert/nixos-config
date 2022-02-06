b4d5c889376dc1c6011f93ce526c866a9279686e
Martin Ertsås <martiert@gmail.com>
2022-02-06 20:18:45 +0100

    moghedien: Move definition to own file

--- flake.nix
+++ flake.nix
@@ -43,45 +43,8 @@
         ];
       };
 
-      moghedien = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ({modulesPath, ...}: {
-            nixpkgs.overlays = [
-              (import "${openconnect-sso}/overlay.nix")
-              (import "${martiert}")
-            ];
-
-            imports = [
-              ./machines/laptop.nix
-            ];
-            networking.hostName = "moghedien";
-          })
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.martin = {
-              imports = [
-                ./home-manager/all.nix
-              ];
-
-              martiert = {
-                i3status = {
-                  enable = true;
-                  wireless = {
-                    wlp1s0 = 1;
-                  };
-                  battery = true;
-                };
-                i3 = {
-                  enable = true;
-                  barSize = 10.0;
-                };
-              };
-            };
-          }
-        ];
+      moghedien = import ./hosts/moghedien.nix {
+        inherit nixpkgs home-manager openconnect-sso martiert;
       };
     };
   };
