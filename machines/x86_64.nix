435cb9ef417ee07a0fa9f2946f6e1e2504f5c87d
Martin Ertsås <martiert@gmail.com>
2022-02-09 12:28:28 +0100

    machines/x86_64: Allow for overriding default kernel modules

--- machines/x86_64.nix
+++ machines/x86_64.nix
@@ -1,26 +1,74 @@
-{ pkgs, lib, ... }:
+{ config, pkgs, lib, ... }:
 
-{
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+with lib;
 
-  services.pcscd.enable = true;
+let
+  bootCfg = config.martiert.boot;
+  hardwareCfg = config.martiert.hardware;
+in {
+  options.martiert.boot = {
+    initrd = mkOption {
+      type = types.submodule {
+        options = {
+          extraAvailableKernelModules = mkOption {
+            type = types.listOf types.str;
+            default = [];
+            description = "Extra kernel module to make available";
+          };
+        };
+      };
+      default = {
+        extraAvailableKernelModules = [];
+      };
+    };
+    kernelModules = mkOption {
+      type = types.listOf types.str;
+      default = [ "kvm-intel" ];
+      description = "Default kernel modules";
+    };
+  };
+  options.martiert.hardware.hidpi = {
+    enable = mkEnableOption "enable hidpi mode";
+  };
 
-  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  config = {
+    boot.initrd.availableKernelModules = [
+      "vfat"
+      "xhci_pci"
+      "ahci"
+      "nvme"
+      "usb_storage"
+      "sd_mod"
+    ] ++ bootCfg.initrd.extraAvailableKernelModules;
 
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "cnijfilter2"
-    "google-chrome"
-    "skypeforlinux"
-    "steam"
-    "steam-original"
-    "steam-runtime"
-    "teamctl"
-    "roomctl"
-    "webex-linux"
-  ];
+    boot.initrd.kernelModules = [ "uas" "usbcore" "usb_storage" "ext4" "nls_cp437" "nls_iso8859_1" ];
+    boot.kernelModules = bootCfg.kernelModules;
+    boot.extraModulePackages = [ ];
 
-  hardware.enableRedistributableFirmware = true;
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
 
-  services.udev.packages = [ pkgs.projecteur ];
+    services.pcscd.enable = true;
+
+    boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
+    nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (getName pkg) [
+      "cnijfilter2"
+      "google-chrome"
+      "skypeforlinux"
+      "steam"
+      "steam-original"
+      "steam-runtime"
+      "teamctl"
+      "roomctl"
+      "webex-linux"
+    ];
+
+    hardware.enableRedistributableFirmware = true;
+    powerManagement.cpuFreqGovernor = mkDefault "powersave";
+    hardware.cpu.intel.updateMicrocode = mkDefault true;
+    hardware.video.hidpi.enable = mkDefault hardwareCfg.hidpi.enable;
+
+    services.udev.packages = [ pkgs.projecteur ];
+  };
 }
