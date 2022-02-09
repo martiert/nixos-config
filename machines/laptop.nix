a65b784e0ebfd21534f411f0e46837a1348ae8a0
Martin Ertsås <martiert@gmail.com>
2022-02-09 12:24:53 +0100

    Move stuff from machine config to host

--- machines/laptop.nix
+++ machines/laptop.nix
@@ -18,16 +18,4 @@ in
   boot.initrd.kernelModules = [ "uas" "usbcore" "usb_storage" "ext4" "nls_cp437" "nls_iso8859_1" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
-
-  martiert.mountpoints = {
-    keyDisk.keyFile = "luks/moghedien.key";
-    root = {
-      encryptedDevice = "/dev/disk/by-uuid/90041fed-c9a4-4139-a61d-76c6c4aca100";
-      device = "/dev/disk/by-uuid/747150e3-46c0-41a2-8735-3c042dec1d2d";
-    };
-    boot = "/dev/disk/by-uuid/C414-3256";
-    swap = "/dev/disk/by-partuuid/813b7f11-8581-4af9-839c-c46e0be03f39";
-  };
-
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 }
