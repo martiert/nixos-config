e367133437c1bd856dec0b48a9147578d5bb73c0
Martin Ertsås <martiert@gmail.com>
2022-02-16 08:48:53 +0100

    machines/rpi: Remove obsolete loader.raspberryPi

--- machines/rpi3.nix
+++ machines/rpi3.nix
@@ -1,14 +1,9 @@
 {
   boot = {
     kernelModules = [ "bcm2835-v4l2" ];
-    loader.raspberryPi = {
-      enable = true;
-      version = 3;
-      uboot.enable = true;
-      firmwareConfig = ''
-        start x=1
-        gpu_mem=256
-      '';
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
     };
   };
   hardware.enableRedistributableFirmware = true;
