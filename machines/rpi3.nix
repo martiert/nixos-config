ee83792d292a9d4032c13d0e73edd0818e129596
Martin Ertsås <martiert@gmail.com>
2022-02-21 23:01:37 +0100

    machines/rpi: Add moghedien as builder

--- machines/rpi3.nix
+++ machines/rpi3.nix
@@ -10,5 +10,6 @@
   nix.settings.trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "moridin.martiert.com:MpOYdKDwUz4u8UpSJGxGUR3Xj40RPJRIvDW9b0vUM6o="
+    "moghedien.martiert.com:5JJbyXsIZrlivMr0UinqJ+ql6QprHcjWjDqyCsJhHJg="
   ];
 }
