a6c425c2009f3c4f450a45e9f91cac036718736e
Martin Ertsås <martiert@gmail.com>
2022-02-21 20:29:10 +0100

    rpi3: Add moridin public key as trusted key

--- machines/rpi3.nix
+++ machines/rpi3.nix
@@ -7,4 +7,8 @@
     };
   };
   hardware.enableRedistributableFirmware = true;
+  nix.settings.trusted-public-keys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "moridin.martiert.com:MpOYdKDwUz4u8UpSJGxGUR3Xj40RPJRIvDW9b0vUM6o="
+  ];
 }
