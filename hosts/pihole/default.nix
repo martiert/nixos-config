4223b6f154da60ac9ca30f76c99720c3b108a863
Martin Ertsås <martiert@gmail.com>
2022-02-21 21:05:12 +0100

    pihole: Add public ssh keys

--- hosts/pihole/default.nix
+++ hosts/pihole/default.nix
@@ -18,6 +18,14 @@ in {
 
     networking.hostName = "pihole2";
     martiert = {
+      sshd = {
+        enable = true;
+        authorizedKeyFiles = [
+          ../../public_keys/pihole/moghedien.pub
+          ../../public_keys/pihole/moridin.pub
+          ../../public_keys/pihole/perrin.pub
+        ];
+      };
       networking.wireless = {
         enable = true;
         interfaces = [ "wlan0" ];
