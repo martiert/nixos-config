b0abf6c2603bdcc83100f241394b38f1562d10d3
Martin Ertsås <martiert@gmail.com>
2022-02-28 23:51:25 +0100

    home-manager: Add tmate to all hosts
    Have to downgrade to 2.3.0, same version as the server

--- home-manager/all.nix
+++ home-manager/all.nix
@@ -44,5 +44,6 @@
     roomctl
 
     projecteur
+    tmate
   ];
 }
--- overlay/default.nix
+++ overlay/default.nix
@@ -11,4 +11,14 @@ self: super: {
   projecteur = martiert.outputs.packages."${system}".projecteur;
   mutt-ics = martiert.outputs.packages."${system}".mutt-ics;
   generate_ssh_key = martiert.outputs.packages."${system}".generate_ssh_key;
+
+  tmate = super.tmate.overrideAttrs (old: rec {
+    version = "2.3.0";
+    src = super.fetchFromGitHub {
+      owner  = "tmate-io";
+      repo   = "tmate";
+      rev    = "2.3.0";
+      sha256 = "SocdTFLGsojBR5+AXQ24x9P97dD8JHImRtfJcGeFmDs=";
+    };
+  });
 }
