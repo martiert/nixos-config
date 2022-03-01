c0321a7bc180c47d1507efbf35d8c144ffc263e9
Martin Ertsås <martiert@gmail.com>
2022-03-01 09:42:00 +0100

    Add new nix store public keys

--- machines/rpi3.nix
+++ machines/rpi3.nix
@@ -11,5 +11,7 @@
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "moridin.martiert.com:MpOYdKDwUz4u8UpSJGxGUR3Xj40RPJRIvDW9b0vUM6o="
     "moghedien.martiert.com:5JJbyXsIZrlivMr0UinqJ+ql6QprHcjWjDqyCsJhHJg="
+    "aginor.martiert.com:ghjjAbho+lr6iyoPOxxBQWOf/bgR1ao87VLN9L4K/EU="
+    "perrin.martiert.com:bdteAJqcaMttOeurDxGiPDsy3gf3q5+LaPrY/wyouOk="
   ];
 }
