20ab108939a9376cf97de00b06200da9933854d7
Martin Ertsås <martiert@gmail.com>
2022-03-02 12:20:15 +0100

    Move nixos cache config into its own file
    This is to only define it one place

--- machines/rpi3.nix
+++ machines/rpi3.nix
@@ -7,11 +7,8 @@
     };
   };
   hardware.enableRedistributableFirmware = true;
-  nix.settings.trusted-public-keys = [
-    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    "moridin.martiert.com:MpOYdKDwUz4u8UpSJGxGUR3Xj40RPJRIvDW9b0vUM6o="
-    "moghedien.martiert.com:5JJbyXsIZrlivMr0UinqJ+ql6QprHcjWjDqyCsJhHJg="
-    "aginor.martiert.com:ghjjAbho+lr6iyoPOxxBQWOf/bgR1ao87VLN9L4K/EU="
-    "perrin.martiert.com:bdteAJqcaMttOeurDxGiPDsy3gf3q5+LaPrY/wyouOk="
+
+  imports = [
+    ./nixos-cache.nix
   ];
 }
