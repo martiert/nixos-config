527f5999d66f2a3156a1d4713e9bfc82c6ab8774
Martin Ertsås <martiert@gmail.com>
2023-02-01 10:29:27 +0100

    Update the aarch64-installer
    Not sure why it still won't load the kernel

--- tools/aarch64-installer/default.nix
+++ tools/aarch64-installer/default.nix
@@ -1,33 +1,31 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
-{
-  boot.kernelPackages = pkgs.linuxPackages_6_0;
+let
+  dtbName = "sc8280xp-lenovo-thinkpad-x13s.dtb";
+in {
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelParams = [
+    "dtb=/boot/aarch64/${dtbName}"
+  ];
+  isoImage.contents = [
+    {
+      source = "${config.boot.kernelPackages.kernel}/dtbs/qcom/${dtbName}";
+      target = "boot/aarch64/${dtbName}";
+    }
+  ];
   hardware.deviceTree = {
     enable = true;
   };
 
   environment.systemPackages = with pkgs; [
     git
-    git-crypt
-    gnupg
   ];
-
-  programs.gnupg.agent = {
-    enable = true;
-    pinentryFlavor = "tty";
-  };
 
   networking.wireless = {
     enable = true;
   };
 
-  environment.etc."gnupg/keys.pub".source = ../../settings/home-manager/keys.pub;
-
-  environment.loginShellInit = ''
-    ${pkgs.gnupg}/bin/gpg --import /etc/gnupg/keys.pub
-  '';
-
-  system.stateVersion = "22.05";
+  system.stateVersion = "23.05";
 
   nix.package = pkgs.nixUnstable;
   nix.extraOptions = ''
