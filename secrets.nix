f3e6f5290c0720bf39e73aa06893c8baa5d615ec
Martin Ertsås <martiert@gmail.com>
2023-07-20 08:46:13 +0200

    Swap out all keys used by schnappi

--- secrets.nix
+++ secrets.nix
@@ -4,7 +4,7 @@ let
   aginor = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMRjg84Y2jgL/qFgc0BPnZvjLkN/fnsDTdLyFfbK+KmZ root@Aginor";
   moghedien = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAlDQcKcShLLVDXOLzzHKx7D6gNetKxC2nL7nFz6SWtu root@moghedien";
   mattrim = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB0jcSi9N1xUK9BHLthykIgI8Wj8/yFdMLdqk5KwL1Hp root@mattrim";
-  schnappi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA6+CqIPHZ+wLsdGv6glma/E9DIH9L0VU8lSs3dEEz4S root@schnappi";
+  schnappi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDA40XbaYVw5sQN25PuEnfahpb4OO3XChh53jk18zkIg root@schnappi";
 
   octoprint = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKU1fdUX2EF8GAH6e6K9gp42XgBjhtrUNYz6kKfHwPpD root@octoprint";
   pihole = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMizRj3YEyAbCL3T9S8Fa2IvSN2Ia/U1hD2ItEzALhZI root@pihole";
@@ -16,6 +16,7 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAtv8ELid3Rl95HFtmE55gXyZiO0Hh3RNmwnDYVU2Szb martin@perrin"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKHTWbjpb92qz5YurxsbdV2PzjdN8TZh4S91AXqYCU1s martin@moghedien"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILuFi4JSDhCe/aqTxoL5zt8r45laYRq+kEvGNRDd8REW martin@aginor"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIRw8XCxKsXXpmEvWGdP/edHvfcNhRTnmj/rrdNp+cqM martin@schnappi"
   ];
 in {
   "secrets/wpa_supplicant_wired.age".publicKeys = [ perrin moridin ] ++ editKeys;
