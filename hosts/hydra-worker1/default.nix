8883348a6d571520bfb7fc2bf846b612472d9043
Martin Ertsås <mertsas@cisco.com>
2024-01-31 13:30:35 +0100

    Make some initial hydra servers
    Currently they will use virtualbox, so we can set it up properly before deploying

---
+++ hosts/hydra-worker1/default.nix
@@ -0,0 +1,30 @@
+{ nixpkgs, ... }:
+
+{
+  system = "x86_64-linux";
+  deployTo = "hydra-worker1";
+
+  nixos = ({ modulesPath, ... }: {
+    imports = [
+      "${toString modulesPath}/virtualisation/virtualbox-image.nix"
+    ];
+
+    martiert = {
+      system = {
+        type = "server";
+      };
+      networking.interfaces = {
+        "enp0s3" = {
+          enable = true;
+          useDHCP = true;
+        };
+      };
+      sshd = {
+        enable = true;
+        authorizedKeyFiles = [
+          ./public_keys/aginor.pub
+        ];
+      };
+    };
+  });
+}
