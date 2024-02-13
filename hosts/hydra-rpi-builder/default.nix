ce0e667fb132bd0901f5ecb37a5031d8102844a0
Martin Ertsås <martiert@gmail.com>
2024-02-13 10:18:41 +0100

    Make a rpi builder for hydra

---
+++ hosts/hydra-rpi-builder/default.nix
@@ -0,0 +1,36 @@
+{ nixpkgs, nixos-hardware, ... }:
+
+{
+  system = "aarch64-linux";
+  deployTo = "hydra-rpi-worker";
+
+  hw_modules = [ nixos-hardware.nixosModules.raspberry-pi-4 ];
+
+  nixos = ({modulesPath, pkgs, config, ... }: {
+    imports = [
+      "${modulesPath}/installer/sd-card/sd-image-aarch64.nix"
+    ];
+
+    martiert = {
+      system = {
+        type = "server";
+        aarch64 = {
+          arch = "rpi3";
+        };
+      };
+      sshd = {
+        enable = true;
+        authorizedKeyFiles = [
+          ./public_keys/schnappi.pub
+          ./public_keys/perrin.pub
+        ];
+      };
+      networking.interfaces = {
+        "eth0" = {
+          enable = true;
+          useDHCP = true;
+        };
+      };
+    };
+  });
+}
