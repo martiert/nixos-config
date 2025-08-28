d4d87fa789064d77357284c62e55b850c2059c3c
Martin Ertsås <martiert@gmail.com>
2025-08-28 10:08:18 +0200

    schnappo: Switch to systemd-boot

--- hosts/schnappo/hardware.nix
+++ hosts/schnappo/hardware.nix
@@ -24,21 +24,50 @@
     ];
     loader = {
       efi.canTouchEfiVariables = true;
-      grub = {
-        efiSupport = true;
-        extraConfig = ''
-          set recordfail=1
-        '';
-        extraPerEntryConfig = ''
-          insmod gzio
-          insmod ext2
-          insmod part_gpt
-          devicetree /dtbs/${pkgs.linuxPackages_latest.kernel.version}/qcom/x1e78100-lenovo-thinkpad-t14s.dtb
-        '';
-        extraFiles = {
-          "dtbs/${pkgs.linuxPackages_latest.kernel.version}/qcom/x1e78100-lenovo-thinkpad-t14s.dtb" = "${pkgs.linuxPackages_latest.kernel}/dtbs/qcom/x1e78100-lenovo-thinkpad-t14s.dtb";
-        };
+      grub.enable = false;
+      systemd-boot = {
+        enable = true;
+        edk2-uefi-shell.enable = true;
       };
+    };
+    initrd = {
+      extraFirmwarePaths = [
+          "qcom/x1e80100/LENOVO/21N1/adsp_dtbs.elf"
+          "qcom/x1e80100/LENOVO/21N1/adspr.jsn"
+          "qcom/x1e80100/LENOVO/21N1/adsps.jsn"
+          "qcom/x1e80100/LENOVO/21N1/adspua.jsn"
+          "qcom/x1e80100/LENOVO/21N1/battmgr.jsn"
+          "qcom/x1e80100/LENOVO/21N1/cdsp_dtbs.elf"
+          "qcom/x1e80100/LENOVO/21N1/cdspr.jsn"
+          "qcom/x1e80100/LENOVO/21N1/qcadsp8380.mbn"
+          "qcom/x1e80100/LENOVO/21N1/qccdsp8380.mbn"
+          "qcom/x1e80100/LENOVO/21N1/qcdxkmsuc8380.mbn"
+      ];
+      kernelModules = [
+        "nvme"
+        "phy_qcom_qmp_pcie"
+        "phy_qcom_qmp_usb"
+        "hid_multitouch"
+        "i2c_hid_of"
+        "i2c_qcom_geni"
+        "leds_qcom_lpg"
+        "pwm_bl"
+        "qrtr"
+        "pmic_glink_altmode"
+        "gpio_sbu_mux"
+        "phy_qcom_qmp_combo"
+        "gpucc_sc8280xp"
+        "dispcc_sc8280xp"
+        "phy_qcom_edp"
+        "panel_edp"
+        "msm"
+        "qcom_glink_smem"
+        "phy_qcom_edp"
+        "qcom_pon"
+        "qcom_pbs"
+        "qcom_edac"
+        "qcom_spmi_pmic"
+      ];
     };
   };
 
