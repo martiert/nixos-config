240896e43b09f129613b4a6f01218a8542435bb4
Martin Ertsås <martiert@gmail.com>
2026-08-06 20:32:48 +0200

    schnappo: Clean up loaded modules

--- hosts/schnappo/hardware.nix
+++ hosts/schnappo/hardware.nix
@@ -23,11 +23,16 @@
         # Platform communication
         "qrtr"
         "qcom_glink_smem"
+        "pmic_glink"
 
         # PHY drivers
         "phy_qcom_qmp_pcie"
         "phy_qcom_qmp_usb"
         "phy_qcom_qmp_combo"
+        "phy_qcom_eusb2_repeater"
+
+        # Mux controllers
+        "mux_gpio"
 
         # Type-C
         "typec"
@@ -35,20 +40,19 @@
         "ucsi_glink"
         "gpio_sbu_mux"
         "pmic_glink_altmode"
+        "ps883x"
 
         # USB host
+        "dwc3"
         "xhci_hcd"
 
         # HID
         "usbhid"
         "hid_generic"
 
-        # Display
-        "gpucc_sc8280xp"
-        "dispcc_sc8280xp"
-        "phy_qcom_edp"
-        "panel_edp"
-        "msm"
+        # Clock controllers (needed for USB PHY clock deps)
+        "gpucc_x1e80100"
+        "dispcc_x1e80100"
 
         # Input devices
         "hid_multitouch"
@@ -58,7 +62,7 @@
         # Other platform stuff
         "leds_qcom_lpg"
         "pwm_bl"
-        "phy_qcom_edp"
+        "phy_nxp_ptn3222"
         "qcom_pon"
         "qcom_pbs"
         "qcom_edac"
